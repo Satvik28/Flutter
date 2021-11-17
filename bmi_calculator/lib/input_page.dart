30277bb7fb7773fb09566d3669a233d459565c78
satvik42069 <satvikdeshmukh3@gmail.com>
2021-11-17 18:03:23 +0530

    Updated BMI app

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -5,7 +5,13 @@ import 'reusable_card.dart';
 
 const double bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -15,6 +21,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,11 +35,18 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
             child: Row(
-              children: const [
+              children: [
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
-                    cardChild: IconContent(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: const IconContent(
                       genderIcon: FontAwesomeIcons.mars,
                       genderText: 'MALE',
                     ),
@@ -39,8 +54,15 @@ class _InputPageState extends State<InputPage> {
                 ),
                 Expanded(
                   child: ReusableCard(
-                    colour: activeCardColor,
-                    cardChild: IconContent(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    colour: selectedGender == Gender.female
+                        ? activeCardColor
+                        : inactiveCardColor,
+                    cardChild: const IconContent(
                       genderIcon: FontAwesomeIcons.venus,
                       genderText: 'FEMALE',
                     ),
@@ -52,7 +74,6 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: ReusableCard(
               colour: activeCardColor,
-              cardChild: null,
             ),
           ),
           Expanded(
--- bmi_calculator/lib/reusable_card.dart
+++ bmi_calculator/lib/reusable_card.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 class ReusableCard extends StatelessWidget {
   final Color colour;
   final Widget? cardChild;
-  const ReusableCard({required this.colour, this.cardChild}) : super(key: null);
+  final Function? onPress;
+  const ReusableCard({required this.colour, this.cardChild, this.onPress})
+      : super(key: null);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: const EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: () {
+        onPress!();
+      },
+      child: Container(
+        child: cardChild,
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
