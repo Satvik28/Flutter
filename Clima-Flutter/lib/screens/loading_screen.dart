073fb8017bd754d7cad01518d5fc524e4b44c5f3
satvik42069 <satvikdeshmukh3@gmail.com>
2021-11-25 23:18:23 +0530

    Added Api calls

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
+
+const String apiKey = '0f3549a3d7cf73a08f62b46dd79fe1a6';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,26 +11,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+  @override
+  void initState() {
+    getLocation();
+    super.initState();
+  }
+
   void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.medium,
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+    print(latitude);
+    print(longitude);
+    getWeatherData();
+  }
+
+  void getWeatherData() async {
+    http.Response response = await http.get(
+      Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey',
+      ),
     );
-    print(position);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      var data = response.body;
+      var decodedData = jsonDecode(data);
+      String cityName = decodedData['name'];
+      double temperature = decodedData['main']['temp'];
+      String description = decodedData['weather'][0]['description'];
+      print(temperature);
+      print(cityName);
+      print(description);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: const Text(
-            'Get Location',
-          ),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
